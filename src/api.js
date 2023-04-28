49565472c0819cd166e2669c095d9c61d4b9916f
AndikaBN <bintangnursalih275@gmail.com>
2023-04-28 15:03:13 +0800

    test

---
+++ src/api.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const serverless = require('serverless-http');
+
+const app = express();
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const fs = require('fs');
+  fs.readFile('./src/data/data.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+    res.send(data);
+  });
+});
+
+app.use('/.netlify/functions/api', router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
